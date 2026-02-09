1fae8fe4a32fdbcd55f08857af98d25a07a1cee4
rsuntk <rissu.ntk@gmail.com>
2026-02-09 20:05:41 +0700

    mm: Multi-gen LRU: fix misleading indentation warning
    ../mm/vmscan.c:2936:3: warning: misleading indentation; statement is not part of the previous 'if' [-Wmisleading-indentation]
     2936 |                 if (mm_state->head == &mm_list->fifo)
          |                 ^
    ../mm/vmscan.c:2933:2: note: previous statement is here
     2933 |         if (!mm_state->head)
          |         ^
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2959,7 +2959,7 @@ static bool iterate_mm_list(struct lruvec *lruvec, struct lru_gen_mm_walk *walk,
 	if (!mm_state->head)
  		mm_state->head = &mm_list->fifo;
 
-		if (mm_state->head == &mm_list->fifo)
+	if (mm_state->head == &mm_list->fifo)
 		first = true;
 
 	do {
