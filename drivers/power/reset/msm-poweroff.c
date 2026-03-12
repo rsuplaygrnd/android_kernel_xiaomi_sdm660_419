210e34b29521aeccec15789772f00cd8b8926a12
rsuntk <rissu.ntk@gmail.com>
2026-03-12 23:20:42 +0700

    Revert "drivers: power: Properly import missing xiaomi changes"
    This reverts commit 54a18933be4f763e979de7d7b2b31db2ec16b85d.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -62,11 +62,7 @@ static void scm_disable_sdi(void);
  * There is no API from TZ to re-enable the registers.
  * So the SDI cannot be re-enabled when it already by-passed.
  */
-#ifdef CONFIG_MACH_LONGCHEER
-int download_mode = 0;
-#else
-int download_mode = 1;
-#endif
+static int download_mode = 1;
 static struct kobject dload_kobj;
 
 static int in_panic;
@@ -499,10 +495,7 @@ static void msm_restart_prepare(const char *cmd)
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 
-	if (in_panic) {
-		qpnp_pon_set_restart_reason(PON_RESTART_REASON_PANIC);
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
-	} else if (cmd != NULL) {
+	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
@@ -538,17 +531,21 @@ static void msm_restart_prepare(const char *cmd)
 				__raw_writel(0x6f656d00 | (code & 0xff),
 					     restart_reason);
 		} else if (!strncmp(cmd, "edl", 3)) {
-			if (0)
-				enable_emergency_dload_mode();
-			else
-				pr_notice("This command already been disabled\n");
+			enable_emergency_dload_mode();
 		} else {
+#ifdef CONFIG_MACH_LONGCHEER
 			qpnp_pon_set_restart_reason(PON_RESTART_REASON_NORMAL);
+#endif
 			__raw_writel(0x77665501, restart_reason);
 		}
+#ifdef CONFIG_MACH_LONGCHEER
+	} else if (in_panic) {
+		qpnp_pon_set_restart_reason(PON_RESTART_REASON_PANIC);
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 	} else {
 		qpnp_pon_set_restart_reason(PON_RESTART_REASON_NORMAL);
 		__raw_writel(0x77665501, restart_reason);
+#endif
 	}
 
 	flush_cache_all();
